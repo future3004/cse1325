67536db445c41d1485bc1412c33e907a8038dc6f
future3004 <richardmel@outlook.com>
2020-09-07 22:17:08 -0500

    second commit

--- P02/full_credit/main.cpp
+++ P02/full_credit/main.cpp
@@ -3,9 +3,46 @@
 
 int main(){
  srand(time(NULL));
+ int number_dice, die_faces, number_rolls, random_number;
+ int sum = 0;
 
- std::cout<< rand()%6+1 <<std::endl;
+ //std::cout<< rand()%6+1 <<std::endl;
+ std::cout << "Number of dice? : ";
+ std::cin >> number_dice;
+ std::cout << "Number of faces per die? : ";
+ std::cin >> die_faces;
+ std::cout << "Number of rolls? : ";
+ std::cin >> number_rolls;
+
+ //std::cout <<"Number_dice = " << number_dice << std::endl;
+ //std::cout <<"Die_faces = " << die_faces << std::endl;
+ //std::cout <<"Number rolls = " << number_rolls << std::endl;
+ for(int i = 0; i < number_rolls; i++){
+   std::cout << "Roll " << i << ": " << std::endl;
+}
+
+for(int j = 0; j < number_dice; j++){
+   random_number = rand()% die_faces + 1;
+   sum += random_number;
+   std::cout << random_number << " ";
+   
+   
+}
+   std::cout << "Sum: " << sum << std::endl;
+  //std::cout << "Final Sum: " << sum << std::endl;
  
+ /*for(int i = 0; i < number_rolls; i++){
+   std::cout << "Roll " << i << ": ";
+   for(int j = 0; j < number_dice; j++){
+   random_number = rand()% die_faces + 1;
+   sum += random_number;
+   std::cout << random_number + " " 
+             << "Sum: " <<  sum << std::endl;
+   //std::cout << random_number << " " << std::endl;
+  }
+}*/
+ 
+ //std::cout << "Final Sum: " << sum << std::endl;
 
 
  return 0;
